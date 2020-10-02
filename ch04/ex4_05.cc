6f9a9f0406a44cd7c301481c5995ee990a3466b8
iandrc <hao8@protonmail.com>
2020-10-03 00:23:11 +0300

    ch04: add exercises 2-5

---
+++ ch04/ex4_05.cc
@@ -0,0 +1,29 @@
+// 5. The CandyBar structure contains three members.
+// The first member holds the brand name of a candy bar.
+// The second member holds the weight (which may have a fractional part) of the candy bar,
+// and the third member holds the number of calories (an integer value) in the candy bar.
+// Write a program that declares such a structure and creates a CandyBar
+// variable called snack,
+// initializing its members to "Mocha Munch", 2.3, and 350, respectively.
+// The initialization should be part of the declaration for snack.
+// Finally, the program should display the contents of the snack variable.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct CandyBar {
+  string name;
+  double weight;
+  unsigned calories;
+};
+
+
+int main() {
+  CandyBar snack = { "Mocha Munch", 2.3, 350 };
+
+  cout << "The " << snack.name << " snack weights " << snack.weight << " grams "
+       << "and gives you " << snack.calories << " calories." << endl;
+
+  return 0;
+}
